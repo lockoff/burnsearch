12eb836ea8eeec3a2d20c82c662538da44ba8b6b
Kiyan Ahmadizadeh <kahmadizadeh@gmail.com>
2015-07-15 13:53:50 -0700

    Adds search for events by description.
    Adds an endpoint for searching for events by description, to enable the
    front-end interface for this feature.
    
    Side Effects:
    + New method in EventResource.
    
    Testing:
    + New unit test for resource.

--- src/main/java/org/burnsearch/web/rest/EventResource.java
+++ src/main/java/org/burnsearch/web/rest/EventResource.java
@@ -3,30 +3,51 @@ package org.burnsearch.web.rest;
 import com.codahale.metrics.annotation.Timed;
 import org.burnsearch.domain.Event;
 import org.burnsearch.repository.search.EventSearchRepository;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.inject.Inject;
+import java.util.List;
 
 /**
  * Endpoints for accessing information about Burning Man events.
  */
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/events")
 public class EventResource {
 
   @Inject
   private EventSearchRepository eventSearchRepository;
 
-  @RequestMapping(value = "/event/{id}",
+  @RequestMapping(value = "/{id}",
       method = RequestMethod.GET,
       produces = MediaType.APPLICATION_JSON_VALUE
   )
   @Timed
   public Event getEventById(@PathVariable("id") Long id) {
     return eventSearchRepository.findOne(id);
+  }
+
+  @RequestMapping(value = "/search/description",
+      method = RequestMethod.GET,
+      produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  @Timed
+  public List<Event> searchByDescription(@RequestParam(value = "q") String description,
+      @RequestParam(value = "page", defaultValue = "0") int pageNumber,
+      @RequestParam(value = "size", defaultValue = "10") int size) {
+    QueryBuilder queryBuilder = new MatchQueryBuilder("description", description);
+    return eventSearchRepository
+        .search(queryBuilder, new PageRequest(pageNumber, size))
+        .getContent();
   }
 }
--- src/test/java/org/burnsearch/web/rest/EventResourceTest.java
+++ src/test/java/org/burnsearch/web/rest/EventResourceTest.java
@@ -1,12 +1,21 @@
 package org.burnsearch.web.rest;
 
+import com.google.common.collect.Lists;
 import org.burnsearch.domain.Event;
 import org.burnsearch.repository.search.EventSearchRepository;
+import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentMatcher;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.FacetedPage;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -33,5 +42,35 @@ public class EventResourceTest {
 
     Event actualEvent = eventResource.getEventById(1L);
     assertEquals("Endpoint returns wrong event.", expectedEvent, actualEvent);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  public void testSearchByDescription() throws Exception {
+    String descriptionQuery = "Hello world.";
+    QueryBuilder queryBuilder = new MatchQueryBuilder("description", descriptionQuery);
+    PageRequest pageRequest = new PageRequest(0, 10);
+    Event result1 = new Event();
+    result1.setId(1L);
+    result1.setDescription("Hello world.");
+    Event result2 = new Event();
+    result2.setId(1L);
+    result2.setDescription("Goodbye world.");
+    List<Event> expectedSearchResults = Lists.newArrayList(result1, result2);
+    FacetedPage<Event> mockPage = (FacetedPage<Event>) mock(FacetedPage.class);
+
+    ArgumentMatcher<QueryBuilder> queryMatcher = new ArgumentMatcher<QueryBuilder>() {
+      @Override
+      public boolean matches(Object o) {
+        return o instanceof MatchQueryBuilder &&
+            BytesReference.Helper.bytesEqual(queryBuilder.buildAsBytes(),
+                ((MatchQueryBuilder) o).buildAsBytes());
+      }
+    };
+    when(mockRepository.search(argThat(queryMatcher), eq(pageRequest))).thenReturn(mockPage);
+    when(mockPage.getContent()).thenReturn(expectedSearchResults);
+
+    List<Event> actualSearchResults = eventResource.searchByDescription(descriptionQuery, 0, 10);
+    assertEquals("Wrong results list.", expectedSearchResults, actualSearchResults);
   }
 }
