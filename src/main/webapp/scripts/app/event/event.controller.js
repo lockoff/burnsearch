b0c321b662b429ec2b862945147e00fab55176ee
Kiyan Ahmadizadeh <kahmadizadeh@gmail.com>
2015-07-22 22:27:58 -0700

    Add some display logic to controller.

--- src/main/webapp/scripts/app/event/event.controller.js
+++ src/main/webapp/scripts/app/event/event.controller.js
@@ -3,4 +3,19 @@
 angular.module('burnsearchApp')
     .controller('EventController', function($scope, event) {
         $scope.event = event;
+        if (event) {
+            $scope.showInfoTable = event.url || event.hostingCamp || event.locatedAtArt;
+        }
+        if (event.locatedAtArt) {
+            var locationString  = undefined;
+            if (event.locatedAtArt.locationString && event.locatedAtArt.locationString != "Unknown") {
+                locationString = "(" + event.locatedAtArt.locationString + ")";
+            } else {
+                locationString = "";
+            }
+            $scope.event.locatedAtArtDisplay = event.locatedAtArt.name + " " + locationString;
+        }
+        if (!event.description || event.description == 'None') {
+            event.description = "No description available.";
+        }
     });
