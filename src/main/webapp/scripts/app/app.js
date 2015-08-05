62c40e351acaeb699c43794b1ec4b20b928e1f51
Kiyan Ahmadizadeh <kahmadizadeh@gmail.com>
2015-08-04 23:56:01 -0700

    Adds moment timezone data to app.
    This was done in response to warnings appearing on the console
    advising that timezone data should be added.

--- src/main/webapp/scripts/app/app.js
+++ src/main/webapp/scripts/app/app.js
@@ -6,6 +6,7 @@ angular.module('burnsearchApp', ['LocalStorageModule',
     'cgBusy'])
 
     .run(function ($rootScope, $location, $window, $http, $state,  Auth, Principal, ENV, VERSION) {
+        moment.tz.add('America/Los_Angeles|PST PDT|80 70|0101|1Lzm0 1zb0 Op0');
         $rootScope.ENV = ENV;
         $rootScope.VERSION = VERSION;
         $rootScope.$on('$stateChangeStart', function (event, toState, toStateParams) {
