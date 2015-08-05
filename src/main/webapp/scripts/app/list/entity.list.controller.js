ed0a3f73eeec0b3ac3b06cb9110d0e29f68e9694
Kiyan Ahmadizadeh <kahmadizadeh@gmail.com>
2015-08-04 18:06:17 -0700

    Add progress indicators when loading lists.
    Loading lists (search results and plans) involves making an AJAX
    request. Adds the angular-busy library so that a progress indicator is
    displayed while the AJAX request is in progress.
    
    Previous attempts to make this work behaved strangely because AJAX
    requests were being performed in resolve blocks on ui-router
    configuration. Because view rendering doesn't begin until the resolve
    block promises are fulfilled, it was difficult to reliably show a
    progress indicator under all circumstances. For this reason http
    requests have been moved into the entity.list.controller.js.
    
    A whirly indicator is used http://www.css-spinners.com/spinner/whirly
    because hey its fun and seems kind of burning man. The indicator looks a
    touch wonky in safari but only a touch.

--- src/main/webapp/scripts/app/list/entity.list.controller.js
+++ src/main/webapp/scripts/app/list/entity.list.controller.js
@@ -9,14 +9,19 @@ angular.module('burnsearchApp')
             params[entityType + "PageNum"] = ($scope.currentPage - 1);
             $state.go(entityType + mode, params);
         };
+        $scope.$parent.entityTab = entityType;
+        $scope.entityType = entityType;
+        $scope.isLoading = true;
+        $window.scrollTo(0,0);
         $scope.listPromise = $http.get(entityGetUrl).then(
             function(response) {
                 $scope.currentPage = (+$stateParams[entityType + "PageNum"]) + 1;
                 $scope.entities = response.data.content;
                 $scope.totalEntities = response.data.totalItems;
+                $scope.isLoading = false;
+            },
+            function (response) {
+                $scope.isLoading = false;
             }
         );
-        $scope.$parent.entityTab = entityType;
-        $scope.entityType = entityType;
-        $window.scrollTo(0,0);
     });
